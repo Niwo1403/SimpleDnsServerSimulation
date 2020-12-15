3dfaee9b2cc50392995b663640587dc0ed6cc0fc
nicow03 <nicow03@mi.fu-berlin.de>
2020-12-15 12:02:37 +0100

    Corrected Proxy.

--- src/proxy.py
+++ src/proxy.py
@@ -2,6 +2,7 @@
 import requests
 import socket
 # local imports
+from dns.dns_message import DnsMessage
 from logger import logger
 from request_server import RequestServer
 
@@ -18,15 +19,24 @@ class Proxy:
 
     @classmethod
     def _resolve_locally(cls, requested_server: str) -> str:
+        request = cls._create_rec_res_request(requested_server)
+        data = cls._send_rec_res_request(request)
+        resp = DnsMessage.new_dns_response(data.decode())
+        return resp.get_address()
+
+    @classmethod
+    def _send_rec_res_request(cls, request):
         client_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        client_sock.sendto(requested_server.encode(), Proxy.REC_RES_ADDRESS)
+        client_sock.sendto(request.encode(), Proxy.REC_RES_ADDRESS)
         data, _ = client_sock.recvfrom(4096)
+        return data
 
-        # TODO: Get DnsMessage from data and get ip addr. and return it
-
-
-        return ""
+    @classmethod
+    def _create_rec_res_request(cls, requested_server):
+        dns_msg = DnsMessage.new_dns_request()
+        dns_msg.set_req(requested_server, recursion_desired=True)
+        request = dns_msg.build_message()
+        return request
 
     def __init__(self, ip_address: str = "127.0.0.100", port: int = 80):
         self.server = RequestServer(
@@ -37,7 +47,6 @@ class Proxy:
         """
         Handles a HTTP GET request, for which the name is passed
         - e.g. 127.0.0.1/my.domain will look for 'my.domain'.
-
         """
         header = request.split("\n")[0]
         requested_server = header.split(" ")[1][1:]
@@ -48,9 +57,10 @@ class Proxy:
         return resp.text
 
     def run(self) -> None:
-        logger.log("----------\nStarting Proxy", flush=True)
+        logger.log("Proxy:")
         self.server.open_socket()
         self.server.run()
+        logger.flush()
 
     def stop(self) -> None:
         """
